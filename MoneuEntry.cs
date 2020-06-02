4e77db3e502b8bbdaebb53b9dbd157297a5ee478
Александр Северов <severonik@outlook.com>
2020-06-03 01:17:25 +0300

    Добавьте файлы проекта.

---
+++ MoneuEntry.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class MoneuEntry
+    {
+        private double _amount;
+
+        /// <summary>
+        /// Стандартный конструктор
+        /// </summary>
+        public MoneuEntry()
+        {
+            _amount = 0;
+            EntryDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="amount">Сумма записи</param>
+        /// <param name="date">Дата записи</param>
+        public MoneuEntry(double amount, DateTime date)
+        {
+            _amount = amount;
+            EntryDate = date;
+        }
+
+        /// <summary>
+        /// Инициализирует объект с помощью строк
+        /// </summary>
+        /// <param name="amount">Сумма записи</param>
+        /// <param name="date">Дата записи</param>
+        public void InitWithString(string amount, string date)
+        {
+            Double.TryParse(amount, out _amount);
+
+            DateTime dt;
+            DateTime.TryParse(date, out dt);
+            EntryDate = dt;
+        }
+                
+        public override string ToString()
+        {
+            return string.Format("{0} от {1}", _amount, EntryDate.Date);
+        }
+
+
+        /// <summary>
+        /// Определяет, является ли запись доходом
+        /// </summary>
+        public bool IsDebit
+        {
+            get
+            {
+                return (_amount >= 0);
+            }
+
+            set
+            {
+                if (value && _amount < 0)
+                    _amount = -_amount;
+            }
+        }
+            
+
+        /// <summary>
+        /// Сумма записи
+        /// </summary>
+        public double amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        /// <summary>
+        /// Дата записи
+        /// </summary>
+        public DateTime EntryDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneuEntry me = new MoneuEntry();
+
+
+            Console.WriteLine("Привет");
+            Console.Write("Введите сумму: ");
+
+            string amount = Console.ReadLine();
+
+            Console.Write("Введите дату: ");
+            string date = Console.ReadLine();
+
+            me.InitWithString(amount, date);
+
+            Console.WriteLine("Ваша запись: ");
+            Console.WriteLine(me.ToString());
+
+            if (me.IsDebit)
+                Console.WriteLine("У вас новый доход.");
+            else
+                Console.WriteLine("У вас расход");
+        }
+    }
+}
